d7cc652288aa5b3987424cff33e27f33909cf102
FerSoriano <fer8f10@hotmail.com>
2026-05-04 17:15:30 -0600

    feat: implement Post endpoint for Recipes

--- app/api/v1/routers/recipes.py
+++ app/api/v1/routers/recipes.py
@@ -1,6 +1,10 @@
 
 from fastapi import APIRouter, HTTPException, status
 from app.schemas.recipe import RecipeResponse, RecipeCreate
+from typing import Optional
+
+# temp
+from .ingredients import MOCK_INGREDIENTS
 
 
 router = APIRouter(
@@ -52,6 +56,13 @@ MOCK_RECIPES = [
 ]
 
 
+def is_recipe_duplicated(name: str, exclude_id: Optional[int] = None) -> bool:
+    for recipe in MOCK_RECIPES:
+        if recipe["name"].lower() == name.lower() and recipe["id"] != exclude_id:
+            return True
+    return False
+
+
 @router.get("/", response_model=list[RecipeResponse])
 async def get_recipes():
     """
@@ -77,3 +88,36 @@ async def get_recipe_by_id(recipe_id: int):
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Recipe with id '{recipe_id}' not found"
     )
+
+
+@router.post("/", response_model=RecipeResponse, status_code=status.HTTP_201_CREATED)
+async def create_recipe(recipe: RecipeCreate):
+    if is_recipe_duplicated(recipe.name):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Recipe with name '{recipe.name}' already exists."
+        )
+    
+    max_id = max(r["id"] for r in MOCK_RECIPES) + 1 if MOCK_RECIPES else 1
+    new_recipe = recipe.model_dump()  # convert model to dict
+    new_recipe["id"] = max_id
+
+    for ingredient in new_recipe["ingredients"]:
+        found = False
+        for i in MOCK_INGREDIENTS:
+            if i["id"] == ingredient["ingredient_id"]:
+                ingredient["ingredient_name"] = i["name"]
+                ingredient["ingredient_unit"] = i["unit"]
+                found = True
+                break
+
+        if not found:
+            raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Ingredient with ID {ingredient["ingredient_id"]} not found."
+        )
+        
+    MOCK_RECIPES.append(new_recipe)
+
+    return new_recipe
+
