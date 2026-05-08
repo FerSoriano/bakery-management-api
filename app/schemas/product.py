225b5692e1c7887dd6d15b14a97f34ff40053087
FerSoriano <fer8f10@hotmail.com>
2026-05-07 23:49:03 -0600

    fix: update category field to use ProductCategory enum in ProductBase and ProductUpdate schemas

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -14,7 +14,7 @@ class ProductBase(BaseModel):
     name: str
     description: str
     sale_price: float  # admin will select the sale price
-    category: str
+    category: ProductCategory
     recipe_id: int
     is_active: bool = True
 
@@ -27,7 +27,7 @@ class ProductUpdate(BaseModel):
     name: Optional[str] = None
     description: Optional[str] = None
     sale_price: Optional[float] = None
-    category: Optional[str] = None
+    category: Optional[ProductCategory] = None
     recipe_id: Optional[int] = None
     is_active: Optional[bool] = None
 
